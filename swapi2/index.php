9b89f70c05f0bab8b964898050b5ea1971a9a275
Amine <aminemastouri@gmail.com>
2019-08-04 17:35:34 +0200

    chgt css et ajout infos

--- swapi2/index.php
+++ swapi2/index.php
@@ -8,9 +8,10 @@
 
 <body>
 	<div class="container">
+	<h1>Find a Star Wars Character</h1>
 		<form method="post" action="">
 
-			<label for="name"> Enter a name : </label>
+			<label for="name"> <p id="label">Enter a name : </p></label>
 			<input type="text" name="name" id="name">
 
 			<input type="submit" name="submit" value="Search">
@@ -19,6 +20,8 @@
 	</div>
 </body>
 </html>
+
+
 
 <?php
 
@@ -36,13 +39,16 @@ if(isset($_POST['submit']) AND !empty($_POST['name']))
 
 if(empty($infos['results']))
 {
-	echo 'This name doesn\'t exist, try another one !';
+	echo '<p style="color:red;font-size:30px">This name doesn\'t exist, try another one !</p>';
 }
 else
 {
-	echo '<div class="container"><p>';
-	echo ($infos['results'][0]['name']);
-	echo '</p></div>';
+	echo '<div class="info-container">';
+	echo '<h1>'.($infos['results'][0]['name']).'</h1>';
+	echo '<p>birth-date: '.($infos['results'][0]['birth_year']).'</p>';
+	echo '<p>height: '.($infos['results'][0]['height']).'</p>';
+	echo '<p>gender: '.($infos['results'][0]['gender']).'</p>';
+	echo '</div>';
 }
 
 	curl_close($ch);
